12a20bc723c6317fcb827524228e322a8f34221f
GarethMansfield <gareth_mansfield@brown.edu>
2021-04-03 18:50:12 -0400

    Tab layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Button } from '@material-ui/core';
 
 import ChatScreen from './views/ChatScreen';
 import AppHeader from './components/AppHeader';
-import ProfilePage from "./ProfilePage/ProfilePage"
+import ProfilePage from "./ProfilePage/ProfilePage";
+import Login from './Login';
+import Form from './Questions';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <div className="App">
-      <AppHeader />
-      <div style={{marginTop: "10vh"}}>
-        <ProfilePage />
-        {/* <ChatScreen /> */}
-      </div>
+      <Router>
+            <AppHeader />
+            <div style={{marginTop: "10vh"}}>
+            <Switch>
+              <Route path="/login">
+                 <Login />
+              </Route>
+              <Route path="/profile">
+                <ProfilePage />
+              </Route>
+              <Route path="/chat">
+                <ChatScreen />
+              </Route>
+              <Route path="/questions">
+                <Form />
+              </Route>
+              <Route path="/signup">
+                 <SignUp />
+              </Route>
+            </Switch>
+            </div>
+      </Router>
     </div>
   );
 }
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Button, Box, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import {Link, BrowserRouter as Router} from 'react-router-dom';
 
 export default function AppHeader() {
+
   return (
     <div style={{flexGrow: "1"}}>
       <AppBar>
         <Toolbar>
           <div style={styles.container}>
-            <Typography edge="start" variant="h6"> Happen! </Typography>
-            <Button color="inherit">Login</Button>
+              <Typography edge="start" variant="h6"> Happen! </Typography>
+              <Link to="/login" style={{textDecoration: 'none'}}>
+                <Button style={{color: 'white', fontSize: "16px"}} color="inherit">Login</Button>
+              </Link>
+              <Link to="/chat" style={{textDecoration: 'none'}}>
+                <Button style={{color: 'white', fontSize: "16px"}} color="inherit">Chat</Button>
+              </Link>
+              <Link to="/profile" style={{textDecoration: 'none',}}>
+                <Button style={{color: 'white', fontSize: "16px"}} color="default">Profile</Button>
+              </Link>
+              <Link to="/questions" style={{textDecoration: 'none'}}>
+                <Button style={{color: 'white', fontSize: "16px"}} color="default">Questions</Button>
+              </Link>
           </div>
         </Toolbar>
       </AppBar>
