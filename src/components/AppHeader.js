6bda9e8f4c7e62a91d21b9dea73d45111fad94ea
Sreehari <sreehari.rammohan@gmail.com>
2021-04-03 20:56:55 -0400

    changing app headers

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import { Button, Box, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
+import { withStyles } from "@material-ui/core/styles";
+import { useLocation } from 'react-router-dom'
+
+
+const WhiteNavItem = withStyles({
+  root: {
+    color: "#f5f9ff"
+  }
+})(Typography)
+
+const useStyles = makeStyles((theme) => ({
+  navlink: {
+    textDecoration: "none",
+    color: "white"
+  }
+ 
+ 
+}))
 
 export default function AppHeader() {
+  const classes = useStyles();
 
-  const routes = ['login', 'signup', 'chats', 'profilePage', 'logout', 'questionaire'];
+  const location = useLocation();
+
+  //'signup'
+  const routes_public = ['Login'];
+  const routes = ['Questionaire', 'Profile', 'Chats', 'Logout'];
+
   return (
     <div style={{flexGrow: "1"}}>
       <AppBar>
         <Toolbar>
           <div style={styles.container}>
-            <Typography edge="start" variant="h6"> Happen! </Typography>
+            <WhiteNavItem edge="start" variant="h6"> Happen! </WhiteNavItem>
 
-            { routes.map((aName) => 
-              <NavLink to={ `/${aName}` }>
-                <Button variant="contained">
-                  {aName}
-                </Button>
-              </NavLink>
-              )
-            }
+            {location.pathname.includes("Login") || location.pathname.includes("Logout") ? routes_public.map((aName) => 
+              <Typography variant="h6">
+                <NavLink className={classes.navlink} to={ `/${aName}` }>
+                    {aName}
+                </NavLink>
+              </Typography>)
+              :
+              routes.map((aName) => 
+              <Typography variant="h6">
+                <NavLink className={classes.navlink} to={ `/${aName}` }>
+                    {aName}
+                </NavLink>
+              </Typography>)
+              }
           </div>
         </Toolbar>
       </AppBar>
