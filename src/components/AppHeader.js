14fb1d5109fc34da8bd37c4855efc6b456797f55
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-04-03 17:10:58 -0700

    Add header buttons

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Button, Box, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
 
 export default function AppHeader() {
+
+  const routes = ['login', 'signup', 'chats', 'profilePage', 'logout', 'questionaire'];
   return (
     <div style={{flexGrow: "1"}}>
       <AppBar>
         <Toolbar>
           <div style={styles.container}>
             <Typography edge="start" variant="h6"> Happen! </Typography>
-            <Button color="inherit">Login</Button>
+
+            { routes.map((aName) => 
+              <NavLink to={ `/${aName}` }>
+                <Button variant="contained">
+                  {aName}
+                </Button>
+              </NavLink>
+
+              )
+            }
+
           </div>
         </Toolbar>
       </AppBar>
